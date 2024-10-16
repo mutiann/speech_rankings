7a5283f2cecfa1a6b647b3a94bbf261112d9932e
mutiann <hemutiann@gmail.com>
2024-10-16 04:15:01 +0200

    improve presentation & consider the case when the conference at the current year is not held yet

--- export.py
+++ export.py
@@ -11,8 +11,19 @@ def main(args):
     exclude_venue = args.exclude_venue.split(',')
     publs.sort(key=lambda x: -x['tags']['year'])
 
+    venue_appearances = defaultdict(set)
     for publ in publs:
         if not args.year_start <= publ['tags']['year'] <= args.year_end:
+            continue
+        venue_appearances[publ['tags']['venue']].add(publ['tags']['year'])
+    shift_venues = []
+    for venue, years in venue_appearances.items():
+        if len(years) == 0 or max(years) < args.year_end:
+            shift_venues.append(venue)
+
+    for publ in publs:
+        shift = 1 if publ['tags']['venue'] in shift_venues else 0
+        if not args.year_start - shift <= publ['tags']['year'] <= args.year_end:
             continue
         if publ['tags']['venue'] in exclude_venue:
             continue
@@ -45,7 +56,7 @@ def main(args):
     authors.sort(key=lambda x: -len(x['pubs']))
     for i, a in enumerate(authors):
         if 'google' not in a:
-            a['google'] = "https://www.google.com/search?q=scholar " + a['name']
+            a['google'] = "https://www.google.com/search?q=" + a['name'] + " site:scholar.google.com"
         a['dblp'] = "https://dblp.uni-trier.de/pid/" + a['pid']
         for tag in a['tags']:
             a['tags'][tag] = list(a['tags'][tag].items())
@@ -54,7 +65,7 @@ def main(args):
         a['pubs'] = a['pubs'][:args.n_pubs]
         a['rank'] = i + 1
 
-        years = list(a['years'].items())
+        years = list(a['years_dedup'].items())
         years.sort(key=lambda x: -x[-1])
         a['years'] = years[:5]
     authors = authors[args.rank_start: args.rank_end]
@@ -67,8 +78,12 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--year-start', type=int, default=2016, help="Min year")
-    parser.add_argument('--year-end', type=int, default=2023, help="Max year")
+    current_year = datetime.datetime.now().year
+    parser.add_argument('--year-start', type=int, default=current_year - 5, help="Min year")
+    parser.add_argument('--year-end', type=int, default=current_year, help="Max year")
+    parser.add_argument('--year-shift', type=int, default=1,
+                        help="Also consider the N years earlier than year-start if no paper is found at year-end;"
+                             "suitable for cases when the conference of the current year is not yet held")
     parser.add_argument('--author-start-year', type=int, default=1900,
                         help="Only consider authors with their first paper after this year")
     parser.add_argument('--exclude-venue', type=str, default="SSW,ASRU,IWSLT,SLT",
